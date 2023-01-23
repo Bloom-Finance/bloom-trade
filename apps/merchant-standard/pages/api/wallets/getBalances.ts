d06da2fcb39a07f51df1b45f774a4551a05254bb
Alex Fiorenza <alexfiorenza2012@gmail.com>
2023-01-23 13:59:23 -0300

    Modified import in merchant-standard

--- apps/merchant-standard/pages/api/wallets/getBalances.ts
+++ apps/merchant-standard/pages/api/wallets/getBalances.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import type { NextApiRequest, NextApiResponse } from 'next';
-import Connector from '../../../../../packages/bloom-finance-connector/dist/main';
+import Connector from '@bloom-trade/finance-connector';
 import {
   ProviderCredentials,
   Chains,
