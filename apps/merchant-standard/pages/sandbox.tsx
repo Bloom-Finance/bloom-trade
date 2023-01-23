ddbdaeaec05794655d246e9ae5b1c6ddc8b6f4b5
Leonardo Leenen <leonardoleenen@gmail.com>
2023-01-23 16:54:22 -0300

    Magic touch

--- apps/merchant-standard/pages/sandbox.tsx
+++ apps/merchant-standard/pages/sandbox.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { Connect, useBloom } from '@bloom-trade/react-sdk';
 import SecuredPage from '../src/components/layout/securedPage';
+
 const SandboxPage: NextPage = () => {
   const { Transfer } = useBloom({
     useTestnet: {
