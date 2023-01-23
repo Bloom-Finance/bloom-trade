54836d99dbd6d54b32953ee6eaaa087815a5baa8
Leonardo Leenen <leonardoleenen@gmail.com>
2023-01-23 16:57:55 -0300

    Magic Touch

--- packages/bloom-react-sdk/src/index.tsx
+++ packages/bloom-react-sdk/src/index.tsx
@@ -3,4 +3,5 @@ import Scrollbar from './components/Scrollbar'
 import CardSurfaces from './components/Surfaces/Card'
 import useBloom from './hooks/useBloom'
 import Connect from './components/Connect'
+
 export { Table, Scrollbar, CardSurfaces, useBloom, Connect }
