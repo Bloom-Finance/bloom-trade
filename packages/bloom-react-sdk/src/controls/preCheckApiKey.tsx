43a037cddb59f3e657f6f4a43fe2a33df413af6d
Alex Fiorenza <alexfiorenza2012@gmail.com>
2023-02-15 11:00:32 -0300

    Fixed trust wallet linking on mobile

--- packages/bloom-react-sdk/src/controls/preCheckApiKey.tsx
+++ packages/bloom-react-sdk/src/controls/preCheckApiKey.tsx
@@ -5,6 +5,23 @@ import { BloomStore } from '../store/BloomReact'
 
 const withPreCheckStore = (Component: React.ComponentType<IBloomReactProps>) => {
   const PreFetch = (props: IBloomReactProps) => {
+    const deepLink = window.localStorage.getItem('WALLETCONNECT_DEEPLINK_CHOICE')
+    useEffect(() => {
+      if (deepLink) {
+        try {
+          const _deepLink: { name: string; href: string } = JSON.parse(deepLink)
+          if (_deepLink.href === 'https://link.trustwallet.com/wc') {
+            window.localStorage.setItem(
+              'WALLETCONNECT_DEEPLINK_CHOICE',
+              JSON.stringify({ name: 'Trust Wallet', href: 'trust://' }),
+            )
+          }
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+          console.log('TrustWallet force redirect err', err)
+        }
+      }
+    }, [deepLink])
     useEffect(() => {
       if (!props.credentials) {
         throw new Error('Credentials are required')
