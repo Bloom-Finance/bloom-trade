4a1d0a6547de8c2eadd7f30636967a07d46b6e37
Alex Fiorenza <alexfiorenza2012@gmail.com>
2023-02-15 22:54:24 -0300

    Deleted change of deep link to trust://

--- packages/bloom-react-sdk/src/controls/preCheckApiKey.tsx
+++ packages/bloom-react-sdk/src/controls/preCheckApiKey.tsx
@@ -1,29 +1,10 @@
 import React, { useEffect } from 'react'
-import { useAccount } from 'wagmi'
 import { BLOOM_URL_API, BLOOM_URL_API_TEST } from '../@types/GLOBAL'
 import { IBloomReactProps } from '../components/BloomReact'
 import { BloomStore } from '../store/BloomReact'
 
 const withPreCheckStore = (Component: React.ComponentType<IBloomReactProps>) => {
   const PreFetch = (props: IBloomReactProps) => {
-    const { isConnected } = useAccount()
-    useEffect(() => {
-      const deepLink = window.localStorage.getItem('WALLETCONNECT_DEEPLINK_CHOICE')
-      if (isConnected && deepLink) {
-        try {
-          const _deepLink: { name: string; href: string } = JSON.parse(deepLink)
-          if (_deepLink.href === 'https://link.trustwallet.com') {
-            window.localStorage.setItem(
-              'WALLETCONNECT_DEEPLINK_CHOICE',
-              JSON.stringify({ name: 'Trust Wallet', href: 'trust://' }),
-            )
-          }
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (err: any) {
-          console.log('TrustWallet force redirect err', err)
-        }
-      }
-    }, [isConnected])
     useEffect(() => {
       if (!props.credentials) {
         throw new Error('Credentials are required')
